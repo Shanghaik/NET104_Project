ab489523fd622d5fb04b6d3910f0d9c9a7329d13
shanghaik168@gmail.com <Shanghaik1998>
2022-11-16 08:47:06 +0700

    abc

--- NET104_Project/Controllers/HomeController.cs
+++ NET104_Project/Controllers/HomeController.cs
@@ -12,10 +12,11 @@ namespace NET104_Project.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public CuahangDbContext CuahangDbContext;
+        public HomeController(ILogger<HomeController> logger, CuahangDbContext cuahangDbContext)
         {
             _logger = logger;
+            CuahangDbContext = cuahangDbContext;
         }
 
         public IActionResult Index()
@@ -28,6 +29,28 @@ namespace NET104_Project.Controllers
             return View();
         }
 
+        public IActionResult TestChucNang()
+        {
+            return View();
+        }
+
+        public IActionResult TestChucNang2()
+        {
+            return RedirectToAction("HienthiSanpham"); // Điều hướng về action index
+        }
+
+        public IActionResult HienthiSanpham()
+        {
+            List<Sanpham> sanphams = new List<Sanpham>()
+            {
+                new Sanpham{Id = Guid.NewGuid(), Ten = "Bimbim", TrangThai = true },
+                new Sanpham{Id = Guid.NewGuid(), Ten = "Redbull", TrangThai = false },
+                new Sanpham{Id = Guid.NewGuid(), Ten = "TV", TrangThai = true },
+                new Sanpham{Id = Guid.NewGuid(), Ten = "ABC", TrangThai = false },
+                new Sanpham{Id = Guid.NewGuid(), Ten = "1234", TrangThai = true }
+            };
+            return View(sanphams);
+        }
         
     }
 }
